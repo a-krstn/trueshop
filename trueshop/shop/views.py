e9265e67a56f226590c11b91902bc8179061a0c6
Alexandr <art1456@mail.ru>
2024-06-29 18:15:08 +0300

    some small modifications in shop views

--- trueshop/shop/views.py
+++ trueshop/shop/views.py
@@ -15,9 +15,9 @@ class ProductList(generic.ListView):
     # paginate_by = 3
 
     def get_queryset(self):
-        if self.kwargs.get('slug'):
+        if self.kwargs.get('category_slug'):
             return services.filter_objects(Product.objects,
-                                           category__slug=self.kwargs.get('slug'),
+                                           category__slug=self.kwargs.get('category_slug'),
                                            available=True)
         return services.filter_objects(Product.objects,
                                        available=True)
@@ -25,10 +25,11 @@ class ProductList(generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         category = None
-        if self.kwargs.get('slug'):
-            category = services.get_instance_by_unique_field(Category, slug=self.kwargs.get('slug'))
+        if self.kwargs.get('category_slug'):
+            category = services.get_instance_by_unique_field(Category, slug=self.kwargs.get('category_slug'))
             context['title'] = f'Категория: {category.name}'
             context['category'] = category
+            context['categories'] = services.all_objects(Category.objects)
             return context
         context['title'] = 'Все товары'
         context['category'] = category
