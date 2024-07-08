3d26b62fe41887efe896d78166d424d8c2553701
Alexandr <art1456@mail.ru>
2024-07-08 18:12:48 +0300

    added user admin action for export orders in csv-format

--- trueshop/orders/admin.py
+++ trueshop/orders/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.http import HttpResponse
+
+import csv
+import datetime
 
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Административное действие для преобразования заказа в csv-файл
+    """
+
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not
+              field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Экспортировать в CSV'
 
 
 def order_stripe_payment(order_obj):
@@ -35,3 +66,4 @@ class OrderAdmin(admin.ModelAdmin):
                     order_stripe_payment, 'created', 'updated')
     list_filter = ('paid', 'created', 'updated')
     inlines = (OrderItemInline,)
+    actions = (export_to_csv,)
